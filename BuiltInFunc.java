dfcb4553f7d2fa9214221e7db073ea4c0b118591
Rajesh Raghavakurup <rtraghavakurup@paypal.com>
2015-10-15 15:22:21 -0700

    minor correction

--- BuiltInFunc.java
+++ BuiltInFunc.java
@@ -1,12 +1,11 @@
 class BuiltInFunc {
 
-	private static final int Size = 10;
 	private static final int Arr[] = { 8, 1, 7, 2, 6, 3, 5, 4, 9, 0 };
 	
 	public static void main(String[] args) {
 		
-		for(int i = 0; i < Size; ++i) {
-			for(int j = i+1; j < Size; ++j) {
+		for(int i = 0; i < Arr.length; ++i) {
+			for(int j = i+1; j < Arr.length; ++j) {
 				if(Arr[i] > Arr[j]) {
 					int temp = Arr[i];
 					Arr[i] = Arr[j];
