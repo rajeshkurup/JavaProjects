8e1ea7d1bfb39dbc1da5550d20b0339f35b62a32
Rajesh Raghavakurup <rtraghavakurup@paypal.com>
2015-10-16 11:05:04 -0700

    oops sample - adding more stuff

--- oops/Driver.java
+++ oops/Driver.java
@@ -3,34 +3,31 @@ import animal.*;
 class Driver {
 
 	public static void main(String[] args) {
-		Animal animal = new Animal();
-		System.out.println(animal);
-		System.out.println(animal.talk());
+		Animal[] animals = new Animal[3];
 
-		Cat cat = new Cat();
-		cat.setName("Bob");
-		cat.setVoice("Meou");
-		cat.setLegs(4);
-		cat.setRunningSpeedMph(5);
-		System.out.println(cat);
-		System.out.println(cat.talk());
+		animals[0] = new Cat();
+		animals[0].setName("Bob");
+		animals[0].setVoice("Meou");
+		animals[0].setLegs(4);
+		animals[0].setRunningSpeedMph(5);
 
-		Dog dog = new Dog();
-		dog.setName("Jimmy");
-		dog.setVoice("Wof");
-		dog.setLegs(4);
-		dog.setRunningSpeedMph(10);
-		System.out.println(dog);
-		System.out.println(dog.talk());
+		animals[1] = new Dog();
+		animals[1].setName("Jimmy");
+		animals[1].setVoice("Wof");
+		animals[1].setLegs(4);
+		animals[1].setRunningSpeedMph(10);
 
-		Kangaroo kangaroo = new Kangaroo();
-		kangaroo.setName("Roger");
-		kangaroo.setVoice("Hehe");
-		kangaroo.setLegs(2);
-		kangaroo.setHands(2);
-		kangaroo.setRunningSpeedMph(15);
-		System.out.println(kangaroo);
-		System.out.println(kangaroo.talk());	
+		animals[2] = new Kangaroo();
+		animals[2].setName("Roger");
+		animals[2].setVoice("Hehe");
+		animals[2].setLegs(2);
+		animals[2].setHands(2);
+		animals[2].setRunningSpeedMph(15);
+
+		for(Animal animal : animals) {
+			System.out.println(animal);
+			System.out.println(animal.talk());
+		}
 	}
 
 }
