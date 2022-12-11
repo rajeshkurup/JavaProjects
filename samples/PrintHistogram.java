6764e7e19887dd6be1b9d6ae86aa6073851bbe4f
Rajesh Raghavakurup <rajeshraghavakurup@Rajeshs-Air.attlocal.net>
2022-12-10 22:51:35 -0800

    adding sample program ideas

---
+++ samples/PrintHistogram.java
@@ -0,0 +1,20 @@
+package samples;
+
+public class PrintHistogram {
+
+    public static void main(String[] args) {
+        System.out.println("================= Program to Print Histogram ===============");
+    }
+
+    // Read data from histogram_data.txt
+    // Print histogram in follwing way
+
+    /**
+     * 01/12/2022 | ====
+     * 02/12/2022 | =========
+     * 03/12/2022 | ===============
+     */
+    
+     // Each "=" represents 1 point
+     // If decimal point value is greater than or equal to 5 add "=" otherwise ignore
+}
---
+++ samples/SampleDatabase.java
@@ -0,0 +1,14 @@
+package samples;
+
+public class SampleDatabase {
+
+    public static void main(String[] args) {
+        System.out.println("================= Sample Database Program ===============");
+    }
+    
+    // Build database operations read, insert, update and delete
+    // Use sample_database.txt file for database storage
+    // First line of the database would always be the number of records in the database
+    // Each record would have 5 fields like First Name, Last Name, Age, Position
+    // Fields in Records are separated by a space character
+}
